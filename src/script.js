ae6fe24c3654c7240c59d8d637e64358b8363b1f
MichelleY17 <yanezdayanna47@gmail.com>
2024-05-22 19:35:50 +0200

    rings,accurate dimentions, lights

--- src/script.js
+++ src/script.js
@@ -10,47 +10,54 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-// Saturn body
+
+// texture loader
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('../textures/saturnmap.jpg');
-const geometry = new THREE.IcosahedronGeometry(1, 12);
+
+// Space Background
+// const spaceTexture = textureLoader.load("../texture/Space-PNG-Picture.png");
+const spaceGeometry = new THREE.SphereGeometry(80, 64, 64);
+const spaceMaterial = new THREE.MeshBasicMaterial({
+    // map: spaceTexture,
+    color: 0xffffff,
+    side: THREE.BackSide,
+    wireframe: true,
+});
+const spaceMesh = new THREE.Mesh(spaceGeometry, spaceMaterial);
+scene.add(spaceMesh);
+
+// Saturn body
+// Saturn dimenstions:
+// Saturn's diametry: 120.536 km
+// Saturn's equatorial radius".: 60.268 km
+const texture = textureLoader.load('../textures/8k_saturn.jpg');
+const saturnRadius = 0.60268; // Saturn radius scaled 
+const geometry = new THREE.IcosahedronGeometry(saturnRadius, 12);
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Saturn rings no particles
-// const ringGeometry = new THREE.RingGeometry(1.5, 2, 64);
-// const ringMaterial = new THREE.MeshBasicMaterial({ 
-//     side: THREE.DoubleSide, 
-//     map: textureLoader.load('../textures/saturnringcolor.jpg'),
-//     transparent: true, 
-//     opacity: 0.8 
-// });
-// const ringAlphaTexture = new THREE.DataTexture(
-//     new Uint8Array([255, 255, 255, 0]), 
-//     1, 1, 
-//     THREE.RGBAFormat, 
-//     THREE.UnsignedByteType,
-//     THREE.UVMapping
-// );
-// ringAlphaTexture.needsUpdate = true;
-// ringMaterial.alphaMap = ringAlphaTexture; // Use the gradient as alpha map
+// Saturn rings *****************************************************
+// Constants for Ring Geometry
+// Rings sizes aprox:
+// Ring's total diametry: 275.000 km
+// Ring's outer radius: 137.500 km
+// Ring's inner radius: 127.500 km
+const ringInnerRadius = 1.27500;
+const ringOuterRadius = 1.37500; 
+const ringThickness = 0.08; //is my custom thickness
 
-// const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
-// ringMesh.rotation.x = Math.PI / 2; // Rotate to the correct orientation
-// scene.add(ringMesh);
-// Ring particles (simplified)
+// Saturn's particle outer ring
 const ringParticlesGeometry = new THREE.BufferGeometry();
-const particleCount = 10000; // Adjust for desired density
+const particleCount = 10000; 
 const positions = new Float32Array(particleCount * 3);
 
 for (let i = 0; i < particleCount; i++) {
-    // Randomly distribute particles within ring boundaries
-    const angle = Math.random() * 2 * Math.PI;
-    const radius = 1.5 + Math.random() * 0.5; // Inner radius + random distance
+    const angle = Math.random() * Math.PI * 2;
+    const radius = ringOuterRadius - Math.random() * (ringOuterRadius - ringInnerRadius) * 0.40; // Adjusted for outer ring
     const x = radius * Math.cos(angle);
     const z = radius * Math.sin(angle);
-    const y = 0; // Assuming ring plane is on the XZ plane
+    const y = (Math.random() - 0.5) * ringThickness;
 
     positions[i * 3] = x;
     positions[i * 3 + 1] = y;
@@ -58,11 +65,34 @@ for (let i = 0; i < particleCount; i++) {
 }
 
 ringParticlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-const ringParticlesMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.01 }); // Adjust size as needed
+const ringParticlesMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.0001 });
 const ringParticles = new THREE.Points(ringParticlesGeometry, ringParticlesMaterial);
 scene.add(ringParticles);
+
+// Saturn's particle inner ring
+const smallRingParticleCount = 10000;
+const positionsSmallRing = new Float32Array(smallRingParticleCount * 3); 
+const smallRingParticlesGeometry = new THREE.BufferGeometry();
+
+for (let i = 0; i < smallRingParticleCount; i++) {
+    const angle = Math.random() * Math.PI * 2;
+    const radius = ringInnerRadius + Math.random() * (ringOuterRadius - ringInnerRadius) * 0.5;
+    const x = radius * Math.cos(angle);
+    const z = radius * Math.sin(angle);
+    const y = (Math.random() - 0.5) * ringThickness;
+
+    positionsSmallRing[i * 3] = x;  
+    positionsSmallRing[i * 3 + 1] = y; 
+    positionsSmallRing[i * 3 + 2] = z; 
+}
+
+smallRingParticlesGeometry.setAttribute('position', new THREE.BufferAttribute(positionsSmallRing, 3));
+const smallRingParticlesMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.0001 }); // Same size as outer ring
+const smallRingParticles = new THREE.Points(smallRingParticlesGeometry, smallRingParticlesMaterial);
+scene.add(smallRingParticles);
+
+
 //  Sizes
-//  
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -86,11 +116,26 @@ window.addEventListener('dblclick',()=>{
        document.exitFullscreen()
     }
 })
-/**
- * Camera
- */
+
+//ambient light
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientlight);
+
+//point Light
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.castShadow = true;
+pointLight.shadowCameraVisible = true;
+pointLight.shadowBias = 0.00001;
+pointLight.shadowDarkness = 0.3;
+pointLight.shadowMapWidth = 2048;
+pointLight.shadowMapHeight = 2048;
+pointLight.position.set(-50, 20, -60);
+scene.add(pointLight);
+
+
+// Camara
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 200)
 camera.position.z = 5
 scene.add(camera)
 
@@ -112,31 +157,37 @@ renderer.setSize(sizes.width, sizes.height)
  * Animate
  */
 const clock = new THREE.Clock();
-const saturnRotationPeriod = 10.4; // rotazione saturno
+// Saturn tilt
+const saturnTilt = -26.73 * Math.PI / 180; 
+mesh.rotation.x = saturnTilt; 
+const saturnRotationPeriod = 10.4; 
 const secondsPerHour = 3600; 
 const radiansPerSecond = (2 * Math.PI) / (saturnRotationPeriod * secondsPerHour)
 
 const tick = () =>
-{
+{    
+    // spacebackground rotation
+    // spaceMesh.rotation.y += 0.0002;
+
+    // saturn rotation
     const elapsedTime = clock.getElapsedTime()
     mesh.rotation.y += radiansPerSecond * elapsedTime;
-    const positions = ringParticlesGeometry.attributes.position.array;
-    for (let i = 0; i < particleCount; i++) {
-        const radius = Math.sqrt(positions[i * 3] ** 2 + positions[i * 3 + 2] ** 2);
-        const speed = 0.0001 / radius; 
-        const angle = Math.atan2(positions[i * 3 + 2], positions[i * 3]) + speed * elapsedTime;
 
-        positions[i * 3] = radius * Math.cos(angle);
-        positions[i * 3 + 2] = radius * Math.sin(angle);
-    }
-    ringParticlesGeometry.attributes.position.needsUpdate = true;
+    // Rings rotations*******************************
+    ringParticles.rotation.x = saturnTilt;
+    smallRingParticles.rotation.x = saturnTilt;
+    // Rotations outer ring
+    ringParticles.rotation.y += 0.0002;
+    // Rotation inner ring
+    smallRingParticles.rotation.y += 0.0002;
+
     // Update controls
     controls.update()
 
     // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
+    // Call tick 
     window.requestAnimationFrame(tick)
 }
 
